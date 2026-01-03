19b307f33182caeb21c6513e4ae09013b1ff1a24
Yousef-120 <yousef2008120@gmail.com>
2026-01-03 08:37:17 +0200

    Fixing problems with breadcrumbs

--- src/components/common/Breadcrumbs.jsx
+++ src/components/common/Breadcrumbs.jsx
@@ -1,30 +1,64 @@
-import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { tags } from "../../modules/core";
+import { useOneProduct } from "../../modules/shop";
 
 export default function Breadcrumbs() {
   const location = useLocation();
-  const breadcrumbs = location.state?.breadcrumbs || [{ label: "Home", path: "/" }];
-useEffect(()=>
-{
-  console.log(location)
-})
+  const { id } = useParams();
+  const [alternatePath, setAlternatePath] = useState([]);
+  const breadcrumbs = location.state?.breadcrumbs || alternatePath;
+
+  const { product, fetchOneProduct } = useOneProduct();
+
+  const fixedPath = { label: "Home", path: "/" };
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    if (path.includes("/shop/product") && id) {
+      fetchOneProduct(id);
+    }
+  }, [location.pathname, id]);
+
+  useEffect(() => {
+    if (!product) return;
+
+    setAlternatePath([fixedPath, { label: "Shop", path: "/shop" }, { label: product.name, path: location.pathname }]);
+  }, [product]);
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    if (path.includes("/shop") && !path.includes("/shop/product")) {
+      setAlternatePath([fixedPath, { label: "Shop", path: "/shop" }]);
+      return;
+    }
+
+    if (path.includes("/brands")) {
+      setAlternatePath([fixedPath, { label: "Brands", path: "/brands" }]);
+      return;
+    }
+
+    const tag = tags.find((tag) => path.includes(tag.path));
+    if (tag) {
+      setAlternatePath([fixedPath, { label: tag.name, path: tag.path }]);
+    }
+  }, [location.pathname]);
+
   return (
     <nav className="flex justify-center mb-6">
       <div className="container flex items-center gap-2">
         {breadcrumbs.map((crumb, index) => {
           const isLast = index === breadcrumbs.length - 1;
-          const isHome = crumb.label == "Home"
-          
+          const isHome = crumb.label === "Home";
+
           return (
             <span key={index} className="flex items-center gap-2">
               {isLast && !isHome ? (
                 <span className="text-black font-medium">{crumb.label}</span>
               ) : (
-                <Link
-                  to={crumb.path}
-                  state={{ breadcrumbs: breadcrumbs.slice(0, index + 1) }}
-                  className="hover:opacity-80 text-[16px] leading-[100%]"
-                >
+                <Link to={crumb.path} state={{ breadcrumbs: breadcrumbs.slice(0, index + 1) }} className="hover:opacity-80 text-[16px]">
                   {crumb.label}
                 </Link>
               )}
